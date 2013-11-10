bcb3e58b202a8a3f9aabae1432bbe75b543c5f62
Anibal Bastias Soto <anibal_bastias@hotmail.com>
2013-11-10 16:54:56 -0200

    Actualizacion Activity_princiapl.xml

--- gen/cl/inexcell/sistemadegestion/R.java
+++ gen/cl/inexcell/sistemadegestion/R.java
@@ -15,8 +15,10 @@ public final class R {
     }
     public static final class id {
         public static final int button1=0x7f070001;
-        public static final int menu_settings=0x7f070003;
-        public static final int pr_instalacion=0x7f070002;
+        public static final int button2=0x7f070002;
+        public static final int button3=0x7f070004;
+        public static final int menu_settings=0x7f070005;
+        public static final int pr_instalacion=0x7f070003;
         public static final int textView1=0x7f070000;
     }
     public static final class layout {
@@ -27,10 +29,13 @@ public final class R {
         public static final int activity_principal=0x7f060000;
     }
     public static final class string {
-        public static final int Instalacion=0x7f040003;
         public static final int app_name=0x7f040000;
         public static final int hello_world=0x7f040001;
+        public static final int instalacion=0x7f040003;
         public static final int menu_settings=0x7f040002;
+        public static final int notificar_averia=0x7f040006;
+        public static final int plantas_externas=0x7f040004;
+        public static final int reparacion=0x7f040005;
     }
     public static final class style {
         /** 
