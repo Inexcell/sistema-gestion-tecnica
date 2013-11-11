cae84932be713426360ba744c74a4c1f9347d0e0
Felipe Navarro <felipe.navarrot@gmail.com>
2013-11-11 07:21:08 -0300

    Avance Vista Reparaciones 11-11-2013
    Falta editarla un poco visualmente

--- src/cl/inexcell/sistemadegestion/Reparacion.java
+++ src/cl/inexcell/sistemadegestion/Reparacion.java
@@ -5,14 +5,32 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class Reparacion extends Activity {
 
+	private LinearLayout ll;
+	private TextView categoria, nombre, modelo;
+	private ImageButton next, prev;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_reparacion);
+		
+		ll = (LinearLayout) findViewById(R.id.info_view);
+		categoria = (TextView) findViewById(R.id.category);
+		nombre = (TextView) findViewById(R.id.name);
+		modelo = (TextView) findViewById(R.id.model);
+		next = (ImageButton) findViewById(R.id.next);
+		prev = (ImageButton) findViewById(R.id.prev);
+		
+		prev.setEnabled(false);
+		ll.setVisibility(View.INVISIBLE);		
+		
+				
 	}
 	
 	public void volver(View view) {
@@ -22,5 +40,31 @@ public class Reparacion extends Activity {
         Vibrator vibrator =(Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         vibrator.vibrate(50);
     }
+	
+	public void buscar_info(View view) {
+		ll.setVisibility(View.VISIBLE);
+		// DO NOTHING
+	}
+	
+	public void siguiente(View view) {
+		prev.setEnabled(true);
+		next.setEnabled(false);
+		
+		categoria.setText("Banda Ancha Fija");
+		nombre.setText("Equipo Modem");
+		modelo.setText("Modelo 411 ZZGT");
+		
+		// DO NOTHING
+	}
+	public void anterior(View view) {
+		prev.setEnabled(false);
+		next.setEnabled(true);
+		
+		categoria.setText("Planta Externa");
+		nombre.setText("Par Externo");
+		modelo.setText("Modelo 001 XTGPP");
+		
+		// DO NOTHING
+	}
 
 }
