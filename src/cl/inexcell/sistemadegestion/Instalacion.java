51a4463fba61f1a427855584726f3a9da641eed8
Anibal Bastias Soto <anibal_bastias@hotmail.com>
2013-11-21 19:49:53 -0200

    Actualizacion dia 21.11.2013

--- src/cl/inexcell/sistemadegestion/Instalacion.java
+++ src/cl/inexcell/sistemadegestion/Instalacion.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -19,8 +21,140 @@ public class Instalacion extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_instalacion);
 		
-		ins_telefono = (EditText)findViewById(R.id.ins_telefono);
-        ins_resultado = (TextView)findViewById(R.id.ins_resultado);
+		//ins_telefono = (EditText)findViewById(R.id.ins_telefono);
+        //ins_resultado = (TextView)findViewById(R.id.ins_resultado);
+        
+        Button btnBusquedaCliente = (Button) findViewById(R.id.btnBusquedaCliente);
+		Button btnPlantaExterna = (Button) findViewById(R.id.btnPlantaExterna);
+		Button btnTVSatelital = (Button) findViewById(R.id.btnTVSatelital);
+		Button btnBandaAncha = (Button) findViewById(R.id.btnBandaAncha);
+		Button btnCertificacion = (Button) findViewById(R.id.btnCertificacion);
+
+		View panelBusquedaCliente = findViewById(R.id.panelBusquedaCliente);
+		panelBusquedaCliente.setVisibility(View.VISIBLE);
+		//panelBusquedaCliente.setVisibility(View.GONE);
+
+		View panelPlantaExterna = findViewById(R.id.panelPlantaExterna);
+		panelPlantaExterna.setVisibility(View.GONE);
+
+		View panelTVSatelital = findViewById(R.id.panelTVSatelital);
+		panelTVSatelital.setVisibility(View.GONE);
+		
+		View panelBandaAncha = findViewById(R.id.panelBandaAncha);
+		panelBandaAncha.setVisibility(View.GONE);
+		
+		View panelCertificacion = findViewById(R.id.panelCertificacion);
+		panelCertificacion.setVisibility(View.GONE);
+
+		btnBusquedaCliente.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// DO STUFF
+				View panelBusquedaCliente = findViewById(R.id.panelBusquedaCliente);
+				panelBusquedaCliente.setVisibility(View.VISIBLE);
+
+				View panelPlantaExterna = findViewById(R.id.panelPlantaExterna);
+				panelPlantaExterna.setVisibility(View.GONE);
+
+				View panelTVSatelital = findViewById(R.id.panelTVSatelital);
+				panelTVSatelital.setVisibility(View.GONE);
+				
+				View panelBandaAncha = findViewById(R.id.panelBandaAncha);
+				panelBandaAncha.setVisibility(View.GONE);
+				
+				View panelCertificacion = findViewById(R.id.panelCertificacion);
+				panelCertificacion.setVisibility(View.GONE);
+
+			}
+		});
+
+		btnPlantaExterna.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// DO STUFF
+				View panelBusquedaCliente= findViewById(R.id.panelBusquedaCliente);
+				panelBusquedaCliente.setVisibility(View.GONE);
+
+				View panelPlantaExterna = findViewById(R.id.panelPlantaExterna);
+				panelPlantaExterna.setVisibility(View.VISIBLE);
+
+				View panelTVSatelital = findViewById(R.id.panelTVSatelital);
+				panelTVSatelital.setVisibility(View.GONE);
+				
+				View panelBandaAncha = findViewById(R.id.panelBandaAncha);
+				panelBandaAncha.setVisibility(View.GONE);
+				
+				View panelCertificacion = findViewById(R.id.panelCertificacion);
+				panelCertificacion.setVisibility(View.GONE);
+
+			}
+		});
+
+		btnTVSatelital.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// DO STUFF
+				View panelBusquedaCliente = findViewById(R.id.panelBusquedaCliente);
+				panelBusquedaCliente.setVisibility(View.GONE);
+
+				View panelPlantaExterna = findViewById(R.id.panelPlantaExterna);
+				panelPlantaExterna.setVisibility(View.GONE);
+
+				View panelTVSatelital = findViewById(R.id.panelTVSatelital);
+				panelTVSatelital.setVisibility(View.VISIBLE);
+				
+				View panelBandaAncha = findViewById(R.id.panelBandaAncha);
+				panelBandaAncha.setVisibility(View.GONE);
+				
+				View panelCertificacion = findViewById(R.id.panelCertificacion);
+				panelCertificacion.setVisibility(View.GONE);
+
+			}
+		});
+		
+		btnBandaAncha.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// DO STUFF
+				View panelBusquedaCliente = findViewById(R.id.panelBusquedaCliente);
+				panelBusquedaCliente.setVisibility(View.GONE);
+
+				View panelPlantaExterna = findViewById(R.id.panelPlantaExterna);
+				panelPlantaExterna.setVisibility(View.GONE);
+
+				View panelTVSatelital = findViewById(R.id.panelTVSatelital);
+				panelTVSatelital.setVisibility(View.GONE);
+				
+				View panelBandaAncha = findViewById(R.id.panelBandaAncha);
+				panelBandaAncha.setVisibility(View.VISIBLE);
+				
+				View panelCertificacion = findViewById(R.id.panelCertificacion);
+				panelCertificacion.setVisibility(View.GONE);
+
+			}
+		});
+		
+		btnCertificacion.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				// DO STUFF
+				View panelBusquedaCliente = findViewById(R.id.panelBusquedaCliente);
+				panelBusquedaCliente.setVisibility(View.GONE);
+
+				View panelPlantaExterna = findViewById(R.id.panelPlantaExterna);
+				panelPlantaExterna.setVisibility(View.GONE);
+
+				View panelTVSatelital = findViewById(R.id.panelTVSatelital);
+				panelTVSatelital.setVisibility(View.GONE);
+				
+				View panelBandaAncha = findViewById(R.id.panelBandaAncha);
+				panelBandaAncha.setVisibility(View.GONE);
+				
+				View panelCertificacion = findViewById(R.id.panelCertificacion);
+				panelCertificacion.setVisibility(View.VISIBLE);
+
+			}
+		});
 	}
 	
 	public void desplegar_resultados(View view) {
