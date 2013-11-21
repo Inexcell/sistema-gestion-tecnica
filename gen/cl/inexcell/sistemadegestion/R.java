7b5c6dcbd5daf97a6ef7cf04e71c183242de6778
Anibal Bastias Soto <anibal_bastias@hotmail.com>
2013-11-21 00:21:31 -0200

    Nuevo icono aplicacion

--- gen/cl/inexcell/sistemadegestion/R.java
+++ gen/cl/inexcell/sistemadegestion/R.java
@@ -23,16 +23,18 @@ public final class R {
         public static final int ic_logo1=0x7f020006;
         public static final int ic_logo2=0x7f020007;
         public static final int ic_logo3=0x7f020008;
-        public static final int ic_notificar_averia=0x7f020009;
-        public static final int ic_plantas_externas=0x7f02000a;
-        public static final int ic_reparacion=0x7f02000b;
-        public static final int ic_reparar=0x7f02000c;
-        public static final int instalar=0x7f02000d;
-        public static final int logo=0x7f02000e;
-        public static final int notificar=0x7f02000f;
-        public static final int reparar=0x7f020010;
-        public static final int tabla=0x7f020011;
-        public static final int tabla_principal=0x7f020012;
+        public static final int ic_logo_icon=0x7f020009;
+        public static final int ic_notificar_averia=0x7f02000a;
+        public static final int ic_plantas_externas=0x7f02000b;
+        public static final int ic_reparacion=0x7f02000c;
+        public static final int ic_reparar=0x7f02000d;
+        public static final int instalar=0x7f02000e;
+        public static final int logo=0x7f02000f;
+        public static final int logo_icon=0x7f020010;
+        public static final int notificar=0x7f020011;
+        public static final int reparar=0x7f020012;
+        public static final int tabla=0x7f020013;
+        public static final int tabla_principal=0x7f020014;
     }
     public static final class id {
         public static final int Button01=0x7f080015;
