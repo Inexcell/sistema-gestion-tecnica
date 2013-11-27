c4b7262620be5219afbd52a5f30c20da8afc80fe
Felipe Navarro Torres <felipe.navarrot@gmail.com>
2013-11-27 06:34:46 +0100

    Notificar Averia y Plantas Externas
    Hechas las maquetas de esas dos vistas.
    Hay unos layouts y clases que agregue para q funcione la cosa esta, pero
    son solo maqueta. Salu2

--- src/cl/inexcell/sistemadegestion/Notificar_Averias.java
+++ src/cl/inexcell/sistemadegestion/Notificar_Averias.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.os.Vibrator;
 import android.view.View;
 import android.view.Window;
+import android.widget.Toast;
 
 public class Notificar_Averias extends Activity {
 
@@ -20,6 +21,21 @@ public class Notificar_Averias extends Activity {
 		setContentView(R.layout.activity_notificar_averias);
 	}
 	
+	/** Boton Guardar Información **/
+	public void guardarInformacion (View view){
+		String text = "Información guardada con éxito";
+		Toast t = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+		t.show();
+	}
+	
+	/** Botón Cámara **/
+	public void capturarImagen(View view){
+		String text = "Captura Almacenada";
+		Toast t = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
+		t.show();
+	}
+	
+	/** Boton Volver **/
 	public void volver(View view) {
     	finish();
     	
--- src/cl/inexcell/sistemadegestion/Plantas_Externas.java
+++ src/cl/inexcell/sistemadegestion/Plantas_Externas.java
@@ -1,14 +1,23 @@
 package cl.inexcell.sistemadegestion;
 
+import java.util.List;
+import java.util.Vector;
+
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.Vibrator;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.view.Window;
 
-public class Plantas_Externas extends Activity {
+public class Plantas_Externas extends FragmentActivity {
 
+	private PagerAdapter mPagerAdapter;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -17,6 +26,19 @@ public class Plantas_Externas extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);		
 		
 		setContentView(R.layout.activity_plantas_externas);
+		
+		this.initialisePaging();
+	}
+	
+	private void initialisePaging() {
+		
+		List<Fragment> fragments = new Vector<Fragment>();
+		fragments.add(Fragment.instantiate(this, planta_ext1.class.getName()));
+		fragments.add(Fragment.instantiate(this, planta_ext2.class.getName()));
+		this.mPagerAdapter = new MyPagerAdapter(super.getSupportFragmentManager(), fragments);
+		
+		ViewPager pager = (ViewPager) super.findViewById(R.id.viewpager);
+		pager.setAdapter(this.mPagerAdapter);
 	}
 	
 	public void volver(View view) {
