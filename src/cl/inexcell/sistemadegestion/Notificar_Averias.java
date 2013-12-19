0f86d14040f6547d2cf292772ae3be7d9949ad94
Felipe Navarro Torres <felipe.navarrot@gmail.com>
2013-12-19 12:39:33 -0300

    captura de datos en notificar avería
    Capturo los datos del formulario al presionar "Guardar información", la
    dejo en variables bien representativas xD y se muestran los datos en un
    AlertDialog.

--- src/cl/inexcell/sistemadegestion/Notificar_Averias.java
+++ src/cl/inexcell/sistemadegestion/Notificar_Averias.java
@@ -1,26 +1,47 @@
 package cl.inexcell.sistemadegestion;
 
+import java.io.BufferedInputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.os.Vibrator;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 public class Notificar_Averias extends Activity {
+	private String observacion, objeto;
+	private Bitmap foto;
+	private Spinner s;
+	private EditText et;
+	private ImageButton ib;
+	private String TAG = "Localizar Avería";
+	private Bitmap b = null, bmini = null;
 	private static int TAKE_PICTURE = 1;
 	private static int SELECT_PICTURE = 2;
 	final CharSequence[] opcionCaptura = {
-    		"Tomar Fotografía", "Galerría"
+    		"Tomar Fotografía"
+    };
+	final CharSequence[] SpinnerText = {
+    		"Cable", "Armario","Caja","Tablero"
     };
 	private String name = "";
 	@Override
@@ -32,20 +53,32 @@ public class Notificar_Averias extends Activity {
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		
 		setContentView(R.layout.activity_notificar_averias);
+		
+		s = (Spinner) findViewById(R.id.Spinner02);
+		et = (EditText) findViewById(R.id.editText1);
+		ib = (ImageButton) findViewById(R.id.ibtnImagen);
 		name = Environment.getExternalStorageDirectory() + "/test.jpg";
 	}
 	
 	/** Boton Guardar Información **/
 	public void guardarInformacion (View view){
-		String text = "Información guardada con éxito";
-		Toast t = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
-		t.show();
+		observacion = et.getText().toString();
+		objeto = SpinnerText[s.getSelectedItemPosition()].toString();
+		foto = b;
+		
+		Drawable d = new BitmapDrawable(getResources(), b);
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle(SpinnerText[s.getSelectedItemPosition()]);
+        builder.setMessage(et.getText());
+        builder.setIcon(d);
+        
+        AlertDialog alert = builder.create();
+        alert.show();
 	}
 	
 	/** Botón Cámara **/
 	public void capturarImagen(View view){
 		
-		String text = "Captura Almacenada";
 		AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Escoja una Opcion:");
         builder.setIcon(R.drawable.ic_camera);
@@ -53,7 +86,6 @@ public class Notificar_Averias extends Activity {
             public void onClick(DialogInterface dialog, int item) {
             	Intent intent =  new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             	int code = TAKE_PICTURE;
-            	
             	if (item==TAKE_PICTURE) {
             		
             	    Uri output = Uri.fromFile(new File(name));
@@ -67,8 +99,32 @@ public class Notificar_Averias extends Activity {
         });
         AlertDialog alert = builder.create();
         alert.show();
-		Toast t = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
-		t.show();
+	}
+	
+	@Override 
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+	    if (requestCode == TAKE_PICTURE) {
+	    	if (data != null) {
+	    		if (data.hasExtra("data")) {
+	    		    b = (Bitmap) data.getParcelableExtra("data");
+	    		}
+	    	} else {
+	    			b = BitmapFactory.decodeFile(name);
+	    			
+	    		}
+	    } else if (requestCode == SELECT_PICTURE){
+	    	Uri selectedImage = data.getData();
+	    	InputStream is;
+	    	try {
+	    	    is = getContentResolver().openInputStream(selectedImage);
+	    	    BufferedInputStream bis = new BufferedInputStream(is);
+	    	    b = BitmapFactory.decodeStream(bis);
+	    	    
+	    	} catch (FileNotFoundException e) {}
+	    }
+	    bmini = Bitmap.createScaledBitmap(b, 64, 64, true);
+	    
+	    
 	}
 	
 	/** Boton Volver **/
